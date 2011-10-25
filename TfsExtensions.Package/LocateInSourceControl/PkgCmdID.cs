188e705694978a16af7d40c83f5f04745faa360e
alexcpendleton <alexcpendleton@gmail.com>
2011-10-25 00:06:54 -0400

    Initial checkin

---
+++ TfsExtensions.Package/LocateInSourceControl/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace Microsoft.LocateInSourceControl
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidLocateInSourceControl =        0x100;
+
+
+    };
+}
---
+++ TfsExtensions.Package/LocateInSourceControl/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LocateInSourceControl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("LocateInSourceControl")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("LocateInSourceControl_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d3e19bc9a5cc594d8de10239fb288c6fa2cab7f73542a557302657c39fadd8a2041ae5e4df3fcb54bbc71d4fbd91eb3646055699c9971abf765ff0673c0f5190157edd32da5cc8a42493585b57fff5d471efcbc41b58c1f84a1427a945f6bde26db76a06e725fa4d0363fee2c9c28424a0a3b2eb8f501b5c79d99a3c4f9572a8")]
+[assembly: InternalsVisibleTo("LocateInSourceControl_UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d3e19bc9a5cc594d8de10239fb288c6fa2cab7f73542a557302657c39fadd8a2041ae5e4df3fcb54bbc71d4fbd91eb3646055699c9971abf765ff0673c0f5190157edd32da5cc8a42493585b57fff5d471efcbc41b58c1f84a1427a945f6bde26db76a06e725fa4d0363fee2c9c28424a0a3b2eb8f501b5c79d99a3c4f9572a8")]
